adb5e8573e977ffc22dd66709c90dc1077dd624d
richa <richa@BOOK-IPIOODHQGO>
2024-12-30 16:41:08 +0900

    q9498 switch version commit

--- Baekjun/src/q9498/Main.java
+++ Baekjun/src/q9498/Main.java
@@ -6,7 +6,29 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		if (n >= 90) {
+		
+		switch ((int)n/10) {
+			case 10:
+				System.out.println("A");
+				break;
+			case 9:
+				System.out.println("A");
+				break;
+			case 8:
+				System.out.println("B");
+				break;
+			case 7:
+				System.out.println("C");
+				break;
+			case 6:
+				System.out.println("D");
+				break;
+			default:
+				System.out.println("F");
+				break;
+		}
+		
+		/*if (n >= 90) {
 			System.out.println("A");
 		} 
 		else if (n >= 80) {
@@ -20,7 +42,9 @@ public class Main {
 		}
 		else {
 			System.out.println("F");
-		}
+		}*/
+		
+		
 
 	}
 }
