fef586a1157306b0f47a7519693f005675d0cc43
richa <richa@BOOK-IPIOODHQGO>
2024-12-30 15:15:20 +0900

    teacher comment

--- Baekjun/src/q2741/Main.java
+++ Baekjun/src/q2741/Main.java
@@ -3,17 +3,13 @@ package q2741;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		int num;
-
 		Scanner sc = new Scanner(System.in);
-		num = sc.nextInt();
+		int num = sc.nextInt();
+		sc.close();
 		for(int i = 0; i<num; i++ ) {
 			System.out.println(i+1);
 		}
-		
-		sc.close();
 	}
 
 }
