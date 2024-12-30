1e5631cee2535c260324718384216b31964d6803
richa <richa@BOOK-IPIOODHQGO>
2024-12-30 14:15:11 +0900

    initial commit

---
+++ Baekjun/src/q2741/Main.java
@@ -0,0 +1,17 @@
+package q2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num;
+		
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		for(int i = 0; i<num; i++ ) {
+			System.out.println(i+1);
+		}
+	}
+
+}
