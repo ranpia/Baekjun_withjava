0c3aaac20d6729678d9db626d5388cba0e88fc11
ranpia <richard.kim146@gmail.com>
2025-01-03 16:27:27 +0900

    q3040 commit

---
+++ Baekjun/src/q3040/Main.java
@@ -0,0 +1,40 @@
+package q3040;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int total=0;
+		int[] arr=new int [9];
+		
+		for (int i=0;i<9;i++) {
+			arr[i]=sc.nextInt();
+			total+=arr[i];
+		}
+		sc.close();
+		total-=100;
+		
+		for (int i=0;i<9;i++) {
+			for (int j=0;j<9;j++) {
+				if (arr[i]==0)
+					break;
+				if (arr[j]==0)
+					break;
+				if ((arr[i]+arr[j])==total&&i!=j) {
+					arr[i]=0;
+					arr[j]=0;
+				}
+			}
+		}
+		
+		for (int i=0;i<9;i++) {
+			if (arr[i]!=0) {
+				System.out.println(arr[i]);
+			}
+		}
+
+	}
+
+}
