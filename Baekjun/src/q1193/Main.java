d7b702b6a02b4f45764a3239ca1bf58f569e5fdd
ranpia <richard.kim146@gmail.com>
2025-01-03 14:16:12 +0900

    q1193 commit

--- Baekjun/src/q1193/Main.java
+++ Baekjun/src/q1193/Main.java
@@ -12,15 +12,17 @@ public class Main {
 		int dir = 0; // 0: 오른쪽 1: 왼쪽아래 2: 아래 3: 오른쪽 위
 		int x = 0;
 		int y = 0;
+		int i = 1;
 
 		if (num > 1) {
-			for (int i = 0; i <= num ; i++) {
+			while (i < num) {
 				switch (dir) {
 				case 0:
 					y++;
+					i++;
 					break;
 				case 1:
-					for (; y >=1&&i<=num;) {
+					for (; y >= 1&&i<num;) {
 						x++;
 						y--;
 						i++;
@@ -28,9 +30,10 @@ public class Main {
 					break;
 				case 2:
 					x++;
+					i++;
 					break;
 				case 3:
-					for (; x >=1&&i<=num;) {
+					for (; x >= 1&&i<num;) {
 						y++;
 						x--;
 						i++;
