41dde6918ff85406dab1e3b67da6cb79049812c9
ranpia <richard.kim146@gmail.com>
2025-01-03 17:25:46 +0900

    q3040 commit

--- Baekjun/src/q3040/Main.java
+++ Baekjun/src/q3040/Main.java
@@ -3,7 +3,7 @@ package q3040;
 import java.util.Scanner;
 
 public class Main {
-
+	//이거 말고 백트래킹으로도 풀어보기
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
 		int total=0;
