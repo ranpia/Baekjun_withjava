1eb3161b7c865115a2464cf3fec9689e6d75b635
ranpia <richard.kim146@gmail.com>
2025-01-10 16:30:11 +0900

    q10828 use util.stack version

---
+++ Baekjun/src/q10828ver3/Main.java
@@ -0,0 +1,47 @@
+package q10828ver3;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();
+		Scanner sc = new Scanner(System.in);
+		int line = sc.nextInt();
+		System.out.println();
+		for (int i = 0; i < line; i++) {
+			String order = sc.next();
+			int insert = 0;
+			switch (order) {
+			case "push":
+				insert = sc.nextInt();
+				stack.push(insert);
+				break;
+			case "pop":
+				if(stack.size()>0)
+					System.out.println(stack.pop());
+				else
+					System.out.println(-1);
+				break;
+			case "size":
+				System.out.println(stack.size());
+				break;
+			case "empty":
+				if (stack.isEmpty()==true)
+					System.out.println(1);
+				else
+					System.out.println(0);
+				break;
+			case "top":
+				if (stack.isEmpty()==true)
+					System.out.println(-1);
+				else
+					System.out.println(stack.lastElement());
+				break;
+			}
+		}
+		sc.close();
+	}
+
+}
